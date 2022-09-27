037989b0f0d56aad5b072d7411fdb22d713cfa68
Miguel <blanco.jimenez.1992@gmail.com>
2022-09-27 17:44:25 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,13 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  function updateSubtotal(product) {
+    console.log('Calculating subtotal, yey!');
+  let price = product.querySelector('.price span').innerHTML;
+  let quantity =product.querySelector('.quantity input').value;
+  let subtotal = quantity * price;
+  
+  product.querySelector('.subtotal span').innerHTML =subtotal;
 }
 
 function calculateAll() {
